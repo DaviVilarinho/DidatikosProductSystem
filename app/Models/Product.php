811a14cc5373d2eedf7248a5f42e9700e046cfd8
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-01-21 09:32:51 -0300

    adicionadas Models básicas, fixadas versões e MoneyPhp

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Money\Money;
+
+class Product extends Model
+{
+    protected $primaryKey = 'cod';
+    protected $fillable = ['nome', 'valor', 'estoque', 'cidade_id'];
+
+    public function cidade()
+    {
+        return $this->belongsTo(Cidade::class, 'cidade_id');
+    }
+
+    protected $casts = [
+        'valor' => MoneyCast::class,
+        'estoque' => 'integer',
+    ];
+}
