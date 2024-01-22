39de99c278a0675821de45167e09ac5fd5570e43
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-01-22 20:45:38 -0300

    fix para camelCase

--- tests/Unit/CidadeProductTest.php
+++ tests/Unit/CidadeProductTest.php
@@ -14,19 +14,19 @@ class CidadeProductTest extends TestCase
   public function testAchaProdutosPorCidades()
   {
     $cidade = Cidade::factory()->create();
-    $outracidade = Cidade::factory()->create();
+    $outraCidade = Cidade::factory()->create();
     $product1 = Product::factory()->create(['cidade_id' => $cidade->id]);
     $product2 = Product::factory()->create(['cidade_id' => $cidade->id]);
-    $product3 = Product::factory()->create(['cidade_id' => $outracidade->id]);
+    $product3 = Product::factory()->create(['cidade_id' => $outraCidade->id]);
 
     $this->assertCount(2, $cidade->products);
-    $this->assertCount(1, $outracidade->products);
-    $produtos_da_cidade = $cidade->find($cidade->id)->products;
-    $produtos_da_outra_cidade = $outracidade->find($outracidade->id)->products;
-    $this->assertTrue($produtos_da_cidade->contains($product1));
-    $this->assertFalse($produtos_da_outra_cidade->contains($product1));
-    $this->assertTrue($produtos_da_cidade->contains($product2));
-    $this->assertTrue($produtos_da_outra_cidade->contains($product3));
-    $this->assertFalse($produtos_da_outra_cidade->contains($product2));
+    $this->assertCount(1, $outraCidade->products);
+    $produtosDaCidade = $cidade->find($cidade->id)->products;
+    $produtosDaOutraCidade = $outraCidade->find($outraCidade->id)->products;
+    $this->assertTrue($produtosDaCidade->contains($product1));
+    $this->assertFalse($produtosDaOutraCidade->contains($product1));
+    $this->assertTrue($produtosDaCidade->contains($product2));
+    $this->assertTrue($produtosDaOutraCidade->contains($product3));
+    $this->assertFalse($produtosDaOutraCidade->contains($product2));
   }
 }
